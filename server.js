34093c36fda27cfbec8a99bbb90f8d0d0813e05d
BABER-DELL\baber <baber9@gmail.com>
2018-06-14 23:21:18 -0500

    connections updated

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 
-
-
 const PORT = process.env.PORT || 3000;
 
 // Initialize express for routing
